10fd3b930d17e961c7225294349eed267cda0d72
julian <zS22017046@estudiantes.uv.mx>
2024-05-28 15:14:27 -0600

    structure

---
+++ Diagram2/Main.java
@@ -0,0 +1,8 @@
+package Diagram2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
---
+++ Diagram3/Main.java
@@ -0,0 +1,8 @@
+package Diagram3;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
